212f2ece7a5be6990835cc8c9dec12cb5a828c92
Emmanuel Williams <oluwadamilare.williams@outlook.com>
2022-10-23 18:25:36 +0100

    Update used car sales analysis.sql

--- used car sales analysis.sql
+++ used car sales analysis.sql
@@ -1,19 +1,19 @@
 /*This query shows the number of used cars by Manufacturers*/
-/*SELECT Make,COUNT(Make) AS cars
+SELECT Make,COUNT(Make) AS cars
 FROM used_car_sales
 GROUP BY Make
-ORDER BY cars DESC*/
+ORDER BY cars DESC
 
 /*This counts how many cars were sold in those 3 years*/
-/*SELECT yearsold,COUNT(yearsold) as Carsold
+SELECT yearsold,COUNT(yearsold) as Carsold
 FROM used_car_sales
-GROUP BY yearsold*/
+GROUP BY yearsold
 
 /* This query counts the year of production*/
-/*SELECT Year_, COUNT(Year_) as Makeyear
+SELECT Year_, COUNT(Year_) as Makeyear
 FROM used_car_sales
 GROUP BY Year_
-ORDER BY Makeyear DESC*/
+ORDER BY Makeyear DESC
 
 /**/
 /*This query shows how many drivetypes were sold*/
@@ -31,51 +31,53 @@ ORDER BY Modelcount DESC
 
 
 /**/
-/*SELECT BodyType,COUNT(BodyType) as body_type, SUM(pricesold) as total_sale_price
+SELECT BodyType,COUNT(BodyType) as body_type, SUM(pricesold) as total_sale_price
 FROM used_car_sales
 GROUP BY BodyType
-ORDER BY body_type DESC*/
+ORDER BY body_type DESC
 
 /**/
-/*SELECT yearsold,SUM(pricesold) AS Total_made
+SELECT yearsold,SUM(pricesold) AS Total_made
 FROM used_car_sales
 GROUP BY yearsold
-ORDER BY yearsold*/
+ORDER BY yearsold
 
 /**/
-/*SELECT yearsold,count(pricesold) AS cars_sold
+SELECT yearsold,count(pricesold) AS cars_sold
 FROM used_car_sales
 GROUP BY yearsold
-ORDER BY yearsold*/
+ORDER BY yearsold
 
 /**/
-/*SELECT NumCylinders, COUNT(NumCylinders) AS cylinder_count
+SELECT NumCylinders, COUNT(NumCylinders) AS cylinder_count
 FROM used_car_sales
 GROUP BY NumCylinders
-ORDER BY cylinder_count DESC*/
+ORDER BY cylinder_count DESC
 
 /**/
-/*SELECT NumCylinders, SUM(Mileage) AS cylinder_mileage
+SELECT NumCylinders, SUM(Mileage) AS cylinder_mileage
 FROM used_car_sales
 GROUP BY NumCylinders
-ORDER BY cylinder_mileage DESC*/
+ORDER BY cylinder_mileage DESC
 
 /**/
-/*SELECT BodyType, SUM(Mileage) AS cylinder_mileage
+SELECT BodyType, SUM(Mileage) AS cylinder_mileage
 FROM used_car_sales
 GROUP BY BodyType
-ORDER BY cylinder_mileage DESC*/
+ORDER BY cylinder_mileage DESC
 
-/*SELECT BodyType, COUNT(BodyType) AS body_count
+/**/
+SELECT BodyType, COUNT(BodyType) AS body_count
 FROM used_car_sales
 WHERE  Mileage > 1000 AND Mileage < 10000
 GROUP BY BodyType
-order by body_count DESC*/
+order by body_count DESC
 
-/*SELECT Year_, COUNT(Year_) AS Production_Year
+/**/
+SELECT Year_, COUNT(Year_) AS Production_Year
 FROM used_car_sales
 GROUP BY Year_
-ORDER BY Production_Year DESC*/
+ORDER BY Production_Year DESC
 
 
 
